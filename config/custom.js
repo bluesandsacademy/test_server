22750b3c2b6f29272dade6707c8880126e3cb801
bluesandsacademy <developer@bluesandsacademy.org>
2025-04-29 09:58:32 +0100

    Update custom.js

--- config/custom.js
+++ config/custom.js
@@ -17,5 +17,5 @@ module.exports.custom = {
   // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
   // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
   // …
-  botBaseURL: 'https://tgbot.odystorm.xyz',
+  botBaseURL: 'https://test-server-x9hq.onrender.com',
 }
